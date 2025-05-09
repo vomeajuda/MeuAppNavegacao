ebffe920db366bf7f0f2fb16dbb0e090872467fb
vomeajuda <leogugo9@hotmail.com>
2025-05-09 14:29:09 -0300

    bug fixes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
